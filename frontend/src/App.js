8a0b34f4a815d03f78ace05eb683b492505d8383
prakash gupta <prakash.gupta@spraxa.com>
2023-02-02 20:38:47 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import UserPlaces from "./places/pages/UserPlaces";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UpdatePlace from "./places/pages/UpdatePlace";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" element={<Users />}></Route>
           <Route path="/:userId/places" element={<UserPlaces />}></Route>
           <Route path="/places/new" element={<NewPlace />}></Route>
+          <Route path="/places/:placeId" element={<UpdatePlace />}></Route>
         </Routes>
       </main>
     </Router>
---
+++ frontend/src/places/pages/UpdatePlace.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdatePlace = (props) => {
+  return <div>UpdatePlace</div>;
+};
+
+export default UpdatePlace;
